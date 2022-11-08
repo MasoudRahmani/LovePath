f56a47eb541c7471548e39763cdab02f30c96bc0
Masoud Rahmani <mrem71@gmail.com>
2022-11-08 13:14:03 +0330

    seperation of concept

---
+++ Utility.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LovePath.Util
+{
+    public static class MySerialization
+    {
+        public static string Serialize<T>(T Obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                DataContractJsonSerializer serialiser = new DataContractJsonSerializer(typeof(T));
+                serialiser.WriteObject(ms, Obj);
+                byte[] json = ms.ToArray();
+                return Encoding.UTF8.GetString(json, 0, json.Length);
+            }
+        }
+
+        public static T Deserialize<T>(string Json)
+        {
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(Json)))
+            {
+                DataContractJsonSerializer serialiser = new DataContractJsonSerializer(typeof(T));
+                var deserializedObj = (T)serialiser.ReadObject(ms);
+                return deserializedObj;
+            }
+        }
+    }
+
+    public static class ConsoleUtils
+    {
+
+        public static void CenterConsole()
+        {
+            IntPtr hWin = GetConsoleWindow();
+            RECT rc;
+            GetWindowRect(hWin, out rc);
+            Screen scr = Screen.FromPoint(new Point(rc.left, rc.top));
+            int x = scr.WorkingArea.Left + (scr.WorkingArea.Width - (rc.right - rc.left)) / 2;
+            int y = scr.WorkingArea.Top + (scr.WorkingArea.Height - (rc.bottom - rc.top)) / 2;
+            MoveWindow(hWin, x, y, rc.right - rc.left, rc.bottom - rc.top, false);
+        }
+        public static void MinizeConsole()
+        {
+            IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
+
+            ShowWindow(handle, 6);
+        }
+
+        // P/Invoke declarations Center
+        private struct RECT { public int left, top, right, bottom; }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT rc);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool MoveWindow(IntPtr hWnd, int x, int y, int w, int h, bool repaint);
+
+        /// <summary>
+        /// Minimize Console
+        /// </summary>
+        [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow([In] IntPtr hWnd, [In] int nCmdShow);
+    }
+
+}
