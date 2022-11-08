2e62cf28b2f0044b95213a63a0f76f02cf2b65b8
Masoud Rahmani <mrem71@gmail.com>
2022-11-08 13:14:18 +0330

    config class

---
+++ Config.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace LovePath
+{
+    [DataContract]
+    public class Config
+    {
+        private string _user = "Hidden";
+        private string _xplorerName = "XYplorerPortable.exe";
+        private string _lovePath = @"E:\";
+
+        [DataMember]
+        public string ProgramPath = AppDomain.CurrentDomain.BaseDirectory;
+        [DataMember]
+        public string ConfigFileName = "lovepathconfig.sec";
+        [DataMember]
+        public string User
+        {
+            get => _user;
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new NullReferenceException();
+                else _user = value;
+            }
+        }
+        [DataMember]
+        public string XplorerName
+        {
+            get => _xplorerName;
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new NullReferenceException();
+                else _xplorerName = value;
+            }
+        }
+        [DataMember]
+        public string LovePath
+        {
+            get => _lovePath;
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new NullReferenceException();
+                else _lovePath = value;
+            }
+
+        }
+    }
+
+}
