4da10a9e8a1511ad66cf6021cc4be30752fcef30
Masoud Rahmani <mrem71@gmail.com>
2022-11-21 00:57:58 +0330

    refactor

---
+++ Run.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security;
+
+namespace LovePath
+{
+    static class Run
+    {
+
+        public static bool RunasProcess_API(string filename, string arg, string user_noDomain, SecureString pass)
+        {
+            using (Process cmd = new Process())
+            {
+                try
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        FileName = filename,
+                        //WindowStyle = ProcessWindowStyle.Hidden,
+                        //CreateNoWindow = true,
+                        Arguments = $"\"{arg.Replace(@"\\", @"\")}\"",
+                        RedirectStandardOutput = true,
+                        //RedirectStandardError = true,
+                        UseShellExecute = false,
+                        UserName = user_noDomain,
+                        Password = pass,
+                        LoadUserProfile = true
+
+                    };
+                    cmd.StartInfo = startInfo;
+                    return cmd.Start();
+
+                    //cmd.WaitForExit();
+                }
+                catch (Exception w)
+                {
+                    throw w;
+                }
+            }
+        }
+
+
+        //if (result) ConsoleUtils.MinizeConsole();
+        public static bool RunasProcess_Shell(string command, string arg, string userwithdomain)
+        {
+            using (Process cmd = new Process())
+            {
+                string fullcmd = $"\"{command} \"{@arg}\"\"";
+                string final = $"/c runas /profile /user:{userwithdomain} " + fullcmd;
+                try
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        CreateNoWindow = false,
+                        Arguments = final,
+                        RedirectStandardInput = false,
+                        RedirectStandardOutput = false,
+                        RedirectStandardError = false,
+                        UseShellExecute = false,
+                        LoadUserProfile = true
+                    };
+                    cmd.StartInfo = startInfo;
+
+                    cmd.Start();
+
+                    cmd.WaitForExit();
+
+                    return true;
+                }
+                catch (Exception w)
+                {
+                    Console.WriteLine(w.Message);
+                    return false;
+                }
+            }
+        }
+
+    }
+}
--- Utility.cs
+++ Utility.cs
@@ -21,27 +21,83 @@ namespace LovePath.Util
             return securePass;
         }
 
-        public static void WriteEncryptedFile(string path, string data, bool encrypted = false)
+        public static void WriteFile(string path, string data)
         {
-            var encrypt = (encrypted) ? FileOptions.Encrypted : FileOptions.WriteThrough;
-            var fs = File.Create(path, 4096, encrypt);
-
+            var fs = File.Create(path);
             using (fs)
             {
                 var byt = Encoding.UTF8.GetBytes(data);
                 fs.Write(byt, 0, byt.Length);
             }
         }
-
-        public static void WriteTempFile(string path, string data = "No Data.")
+        public static void TestTempFile(string data)
         {
-            var fs = File.Create(path, 4096, FileOptions.Encrypted);
-            using (fs)
+            var paths = new string[2] {
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "tmp.tmp_lovepath"),
+                "t.tmp"};
+
+            foreach (var path in paths)
             {
-                var byt = Encoding.UTF8.GetBytes(data);
-                fs.Write(byt, 0, byt.Length);
+                var fs = File.Create(path, 4096);
+                using (fs)
+                {
+                    var byt = Encoding.UTF8.GetBytes(data);
+                    fs.Write(byt, 0, byt.Length);
+
+                }
+                File.ReadAllText(path);
             }
         }
+
+
+        public static void ShowExit(string msg = "")
+        {
+            Console.Clear();
+            var newline = string.IsNullOrWhiteSpace(msg) ? string.Empty : Environment.NewLine;
+
+            Console.Write($@"{msg}{newline} Z-> Exit, Y-> Restart: ");
+
+            var key = Console.ReadKey().Key;
+            if (key == ConsoleKey.Z) Environment.Exit(0);
+            if (key == ConsoleKey.Y) Restart();
+
+            Console.Clear();
+        }
+        private static void Restart()
+        {
+            Console.WriteLine("Reboot...");
+            // Starts a new instance of the program itself
+            System.Diagnostics.Process.Start(Path.Combine(Directory.GetCurrentDirectory(), AppDomain.CurrentDomain.FriendlyName));
+
+            // Closes the current process
+            Environment.Exit(0);
+        }
+
+        private static string help;
+        public static void HelpInConsole(ConsoleKey key)
+        {
+            if (key == ConsoleKey.H)
+                help = "h";
+
+            if (help == "h" & key == ConsoleKey.E)
+                help += "e";
+
+            if (help == "he" & key == ConsoleKey.L)
+                help += "l";
+
+            if (help == "hel" & key == ConsoleKey.P)
+                help += "p";
+
+            if (key == ConsoleKey.Help || key == ConsoleKey.End)
+                help = "help";
+
+            if (help == "help")
+            {
+                ShowExit();
+                help = "";
+            }
+        }
+
 
         //https://stackoverflow.com/questions/223952/create-an-instance-of-a-class-from-a-string
         private static object GetClassInstanceOfTypeName(string strFullyQualifiedName, ref Type objType)
